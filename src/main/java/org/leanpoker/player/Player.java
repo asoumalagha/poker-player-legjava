1e7da83fab39fb4e7f0c6af518e333cddeb73e84
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-09-02 11:59:49 +0200

    json elements and arrays implemented, gson added to trash

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,22 +3,28 @@ package org.leanpoker.player;
 import com.google.gson.JsonElement;
 import com.google.gson.*;
 
-import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
-        Gson gson = new Gson();
         JsonObject data = request.getAsJsonObject();
         JsonArray players = data.get("players").getAsJsonArray();
 
-        Map playersList = gson.fromJson(players, Map.class);
+        for(JsonElement player: players){
+            JsonObject jsonObject = player.getAsJsonObject();
+            if(jsonObject.get("name").equals("LegJava")){
+                JsonArray wholeCards = jsonObject.get("hole_cards").getAsJsonArray();
+                for(JsonElement card: wholeCards){
+                    String cardString = card.toString();
+                    Logger.log(cardString);
+                }
+                break;
+            }
 
-        String playersListString = playersList.get("LegJava").toString();
+        }
 
-        Logger.log(playersListString);
 
         return 500;
     }
