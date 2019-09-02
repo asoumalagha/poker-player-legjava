f5f900be926d5402c0978b196609d7faaed99bfe
Alex <alexalpek@gmail.com>
2019-09-02 13:59:24 +0200

    only logging cards

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -17,22 +17,15 @@ public class Player {
             for (JsonElement player : players) {
                 JsonObject playerObject = player.getAsJsonObject();
                 if (playerObject.get("name").getAsString().equals("LegJava")) {
-                    Logger.log(playerObject.get("name").toString());
-                    //Logger.log(playerObject.getAsString());
-                }
-                    /*JsonArray holeCards = playerObject.get("hole_cards").getAsJsonArray();
+                    JsonArray holeCards = playerObject.get("hole_cards").getAsJsonArray();
                     Logger.log(holeCards.getAsString());
-                    for (JsonElement card : holeCards){
+                    for (JsonElement card : holeCards) {
                         Logger.log(card.getAsString());
                     }
                     break;
                 } else {
                     Logger.log("Kiscica");
                 }
-                Logger.log(playerObject.toString());
-            }
-
-                     */
             }
         } catch (Exception e) {
             return 800;
