183467c7757fa028912434011c8883e71f52eb66
lupakd <daniel.lupis@gmail.com>
2019-09-02 10:16:30 +0200

    1st bet

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,9 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 
 import java.util.Map;
 
@@ -9,8 +12,7 @@ public class Player {
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
-
-        return 10;
+        return 20;
     }
 
     public static void showdown(JsonElement game) {
